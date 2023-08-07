3c575478b456ed7e227211b6d405ca96172446f1
ycyeha <109306049@nccu.edu.tw>
2023-08-07 15:16:24 +0800

    20230806
    1.
    把build.gradle (UI Basic)中的8.0.4之類的改成7.4.2
    
    2.
    build.gradle (:app)（for .gif檔案）
    
    3.
    Drawable資料夾中新增fish_gif.gif
    
    4.
    activity_main.xml中新增<pl.droidsonrods.gif.GifImageView>那串
    
    5.
    MainActivity.java中import兩個package、新增fish_gif的變數、加上GifImageView的一大串

--- app/src/main/java/com/example/uibasic/MainActivity.java
+++ app/src/main/java/com/example/uibasic/MainActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+//0806
+import pl.droidsonroids.gif.GifImageView;
+import pl.droidsonroids.gif.GifDrawable;
+
 public class MainActivity extends AppCompatActivity {
+
+    //0806
+    private GifImageView fish_gif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    //0806
+    GifImageView fish_gif = findViewById(R.id.fish_gif);
+    try{
+        GifDrawable gifDrawable = new GifDrawable(getResources(), R.drawable.fish_gif);
+        fish_gif.setImageDrawable(gifDrawable);
+    }catch (Exception e){
+        e.printStackTrace();
+    }
+
     }
 }
