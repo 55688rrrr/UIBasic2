bd6e52bfc80a52d6c2d33952a9bb2d5a9c7b3c2c
ycyeha <109306049@nccu.edu.tw>
2023-08-07 16:56:11 +0800

    20230807_2
    簡單建了一個ArrayList
    
    隨便拉了一個button（應該會在右下角），試著寫新增魚的程式，但不知道哪裡出錯....:( "A resource failed to call close":(
    
    ***新增魚後，要怎麼讓它出現在random的位置？？

--- app/src/main/java/com/example/uibasic/MainActivity.java
+++ app/src/main/java/com/example/uibasic/MainActivity.java
@@ -3,29 +3,53 @@ package com.example.uibasic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 //0806
 import pl.droidsonroids.gif.GifImageView;
 import pl.droidsonroids.gif.GifDrawable;
 
+//0807
+import android.widget.Button;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     //0806
     private GifImageView fish_gif;
+    //0807
+    ArrayList<GifImageView> fishList = new ArrayList<GifImageView>();
+    //增加魚：fishList.add(fish0);
+    //魚死掉之類的：fishList.remove(0);
+    //不同類別，e.g. 運動、健康，可以設成fish_sport_0、fish_health_0之類的？
+
+    //0807
+    Button addFish = findViewById(R.id.addFish);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    //0806
-    GifImageView fish_gif = findViewById(R.id.fish_gif);
-    try{
-        GifDrawable gifDrawable = new GifDrawable(getResources(), R.drawable.fish_gif);
-        fish_gif.setImageDrawable(gifDrawable);
-    }catch (Exception e){
-        e.printStackTrace();
-    }
+        //0806
+        GifImageView fish_gif = findViewById(R.id.fish_gif);
+        try{
+            GifDrawable gifDrawable = new GifDrawable(getResources(), R.drawable.fish_gif);
+            fish_gif.setImageDrawable(gifDrawable);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //0807
+        addFish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //GifImageView fish0;
+                //fishList.add(fish0);
+            }
+        });
 
     }
 }
