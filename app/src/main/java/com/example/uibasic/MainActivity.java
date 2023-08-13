db4aae7e5dfb105cff7033b34fff23f8527c00dd
高昂 <108306009@nccu.edu.tw>
2023-08-13 22:59:13 +0800

    連接資料庫 MainActivity裡面註解的還沒改完

--- app/src/main/java/com/example/uibasic/MainActivity.java
+++ app/src/main/java/com/example/uibasic/MainActivity.java
@@ -10,5 +10,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*final TextView text_view = (TextView) findViewById(R.id.text_view);
+
+        new Thread(new Runnable(){
+            @Override
+            public void run(){
+                MysqlCon con = new MysqlCon();
+                con.run();
+                final String data = con.getData();
+                Log.v("OK",data);
+                text_view.post(new Runnable() {
+                    public void run() {
+                        text_view.setText(data);
+                    }
+                });
+
+            }
+        }).start();*/
     }
 }
