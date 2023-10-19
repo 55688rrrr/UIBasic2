f818a7d1d4c68acefabce6f29b22fd229ee644b3
heya <109306049@nccu.edu.tw>
2023-10-19 12:40:15 +0800

    1019
    1. 把事先寫好的🐟一開始先隱藏，按按鈕才會出現
    
    2. 🐟的位置會隨機出現:D
    （解說有用註解的方式寫在118~121行）

--- app/src/main/java/com/example/uibasic/MainActivity.java
+++ app/src/main/java/com/example/uibasic/MainActivity.java
@@ -18,6 +18,13 @@ import android.widget.ImageButton;
 
 import java.util.ArrayList;
 
+//1018
+import android.view.animation.TranslateAnimation;
+//可是只能extends一個而已... implements要接interface...
+//1019 試著改變位置？
+import android.graphics.Canvas;
+import android.graphics.Bitmap;
+
 public class MainActivity extends AppCompatActivity {
 
     //0806
@@ -40,91 +47,115 @@ public class MainActivity extends AppCompatActivity {
     private GifImageView barrel_gif;
     private GifImageView seaweed_gif_2;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    //1019 inner class執行TranslateAnimation？？
+//    public TranslateAnimation(double fromX, double toX, double fromY, double toY) {
+//        fromX = Math.random();
+//        fromY = Math.random();
 
-        //0806
-        GifImageView fish_gif = findViewById(R.id.fish_gif);
-        //0821
-        GifImageView fish_gif_2 = findViewById(R.id.fish_gif_2);
-        GifImageView bw_fish = findViewById(R.id.bw_fish);
-        //0827
-        GifImageView bw_fish_2 = findViewById(R.id.bw_fish_2);
-        try{
-            GifDrawable gifFish = new GifDrawable(getResources(), R.drawable.blue_fish);
-            fish_gif.setImageDrawable(gifFish);
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            //1019 一開始先隱藏？
+            GifImageView blue_fish_0 = findViewById(R.id.blue_fish_0);
+            blue_fish_0.setVisibility(View.INVISIBLE);
+
+            //0806
+            GifImageView fish_gif = findViewById(R.id.fish_gif);
             //0821
-            GifDrawable gifFish2 = new GifDrawable(getResources(), R.drawable.blue_fish);
-            fish_gif_2.setImageDrawable(gifFish2);
-            GifDrawable bwFish = new GifDrawable(getResources(), R.drawable.black_white_fish);
-            bw_fish.setImageDrawable(bwFish);
+            GifImageView fish_gif_2 = findViewById(R.id.fish_gif_2);
+            GifImageView bw_fish = findViewById(R.id.bw_fish);
             //0827
-            GifDrawable bwFish2 = new GifDrawable(getResources(), R.drawable.black_white_fish);
-            bw_fish_2.setImageDrawable(bwFish2);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+            GifImageView bw_fish_2 = findViewById(R.id.bw_fish_2);
+            try {
+                GifDrawable gifFish = new GifDrawable(getResources(), R.drawable.blue_fish);
+                fish_gif.setImageDrawable(gifFish);
+                //0821
+                GifDrawable gifFish2 = new GifDrawable(getResources(), R.drawable.blue_fish);
+                fish_gif_2.setImageDrawable(gifFish2);
+                GifDrawable bwFish = new GifDrawable(getResources(), R.drawable.black_white_fish);
+                bw_fish.setImageDrawable(bwFish);
+                //0827
+                GifDrawable bwFish2 = new GifDrawable(getResources(), R.drawable.black_white_fish);
+                bw_fish_2.setImageDrawable(bwFish2);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-        //0813
-        GifImageView seaweed_gif = findViewById(R.id.seaweed_gif);
-        GifImageView barrel_gif = findViewById(R.id.barrel_gif);
-        GifImageView seaweed_gif_2 = findViewById(R.id.seaweed_gif_2);
-        try{
-            GifDrawable gifSeaweed = new GifDrawable(getResources(), R.drawable.seaweed_1);
-            GifDrawable gifBarrel = new GifDrawable(getResources(), R.drawable.barrel_1);
-            GifDrawable gifSeaweed2 = new GifDrawable(getResources(), R.drawable.seaweed_1);
-            seaweed_gif.setImageDrawable(gifSeaweed);
-            barrel_gif.setImageDrawable(gifBarrel);
-            seaweed_gif_2.setImageDrawable(gifSeaweed2);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+            //0813
+            GifImageView seaweed_gif = findViewById(R.id.seaweed_gif);
+            GifImageView barrel_gif = findViewById(R.id.barrel_gif);
+            GifImageView seaweed_gif_2 = findViewById(R.id.seaweed_gif_2);
+            try {
+                GifDrawable gifSeaweed = new GifDrawable(getResources(), R.drawable.seaweed_1);
+                GifDrawable gifBarrel = new GifDrawable(getResources(), R.drawable.barrel_1);
+                GifDrawable gifSeaweed2 = new GifDrawable(getResources(), R.drawable.seaweed_1);
+                seaweed_gif.setImageDrawable(gifSeaweed);
+                barrel_gif.setImageDrawable(gifBarrel);
+                seaweed_gif_2.setImageDrawable(gifSeaweed2);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-        //0807          //0925 用主畫面左邊那個button試做成新增魚的按鈕；重做0807的
-        addFish = findViewById(R.id.imageButton3);
-        addFish.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //GifImageView fish0;
-                //fishList.add(fish0);
-                //System.out.print(R.drawable.blue1fish);
-                GifImageView blue_fish_0 = findViewById(R.id.blue_fish_0);
-                try{
-                    GifDrawable blueFish0 = new GifDrawable(getResources(), R.drawable.blue_fish);
-                    blue_fish_0.setImageDrawable(blueFish0);
-                }catch (Exception e){
-                    e.printStackTrace();
+            //0807          //0925 用主畫面左邊那個button試做成新增魚的按鈕；重做0807的
+            addFish = findViewById(R.id.imageButton3);
+            addFish.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //GifImageView fish0;
+                    //fishList.add(fish0);
+                    //System.out.print(R.drawable.blue1fish);
+//                    1019 一開始先宣告了所以這邊註解掉
+//                    GifImageView blue_fish_0 = findViewById(R.id.blue_fish_0);
+                    //1019 一開始先隱藏，點了按鈕之後才會出現   （成功ㄌ！！！:DDD ）
+                    blue_fish_0.setVisibility(View.VISIBLE);
+//                    //1019 試著改變位置？
+//                    Bitmap tryBitmap = blue_fish_0.getDrawingCache();   //後綴是自己跑出來的（？
+//                    Canvas tryCanvas = new Canvas(tryBitmap);
+//                    tryCanvas.drawBitmap(tryBitmap,0,0,null);
+//                    blue_fish_0.draw();
+                        //這邊可以設定位置！不論xml裡面是不是已經設定了
+                        //Math.random()會產生0~1的數，所以再乘一個比較大的數，增加變化
+                        //不過Math.random()產生的是double，setX、setY好像需要的是int？？
+                        //另外還有個問題：之前是硬寫一個gif上去，所以魚的位置如果出現在螢幕右邊，就會直接游出去，不會自動碰到邊界就折返
+                    blue_fish_0.setX((int)(Math.random()*1000));
+                    blue_fish_0.setY((int)(Math.random()*2000));
+                    try {
+                        GifDrawable blueFish0 = new GifDrawable(getResources(), R.drawable.blue_fish);
+                        blue_fish_0.setImageDrawable(blueFish0);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    //0930 （希望有懸浮視窗後可以直接從arraylist看到有幾隻魚？？）
+                    //可以用blueFishList.size()得知list的大小aka魚的數量
+                    blueFishList.add(blue_fish_0);
+                    //❓能不能讓魚出現在隨機的位置？？？😟
                 }
-                //0930 （希望有懸浮視窗後可以直接從arraylist看到有幾隻魚？？）
-                //可以用blueFishList.size()得知list的大小aka魚的數量
-                blueFishList.add(blue_fish_0);
-                //❓能不能讓魚出現在隨機的位置？？？😟
-            }
-        });
+            });
 
-        //0827 按中間那顆可以切到登入畫面
-        ImageButton mdbt = findViewById(R.id.imageButton);
-        mdbt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, login_page.class);
-                startActivity(intent);
-            }
-        });
+            //0827 按中間那顆可以切到登入畫面
+            ImageButton mdbt = findViewById(R.id.imageButton);
+            mdbt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, login_page.class);
+                    startActivity(intent);
+                }
+            });
 
-        //1008 右邊那顆切魚
-        ImageButton rtbt = findViewById(R.id.imageButton2);
-        rtbt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MainActivity3.class);
-                startActivity(intent);
-            }
-        });
+            //1008 右邊那顆切魚
+            ImageButton rtbt = findViewById(R.id.imageButton2);
+            rtbt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, MainActivity3.class);
+                    startActivity(intent);
+                }
+            });
 
-    }
+        }
+//    }
 }
 
 
--- app/src/main/java/com/example/uibasic/MainActivity3.java
+++ app/src/main/java/com/example/uibasic/MainActivity3.java
@@ -3,12 +3,70 @@ package com.example.uibasic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
+
+//1009
+import android.widget.ImageButton;
+import android.content.Intent;
+//1009 試試看？？
+import android.widget.ListView;
+import java.util.ArrayList;
+import android.widget.ArrayAdapter;
 
 public class MainActivity3 extends AppCompatActivity {
+
+    //1009
+    //private ImageButton add_blueFish;
+
+    //1009 試試看？？
+    private ArrayList<GifDrawable> bfList;
+    private ArrayAdapter<GifDrawable> bfList_Adapter;
+    private ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        //1009 試試看？？
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        bfList = new ArrayList<GifDrawable>();
+        bfList_Adapter = new ArrayAdapter<GifDrawable>(this,
+                android.R.layout.simple_list_item_1, bfList);
+        lvItems.setAdapter(bfList_Adapter);
+
+        //1009
+        ImageButton add_blueFish = findViewById(R.id.add_blueFish);
+        add_blueFish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GifImageView blue_fish_0 = findViewById(R.id.blue_fish_0);
+                //1009 試試看的時候自己跑出來...？？？？
+                //GifDrawable blueFish0 = null;
+                try {
+                    GifDrawable blueFish0 = new GifDrawable(getResources(), R.drawable.blue_fish);
+                    blue_fish_0.setImageDrawable(blueFish0);
+                        //1009 試試看？？
+                        bfList.add(blueFish0);
+                        bfList_Adapter.add(blueFish0);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                Intent intent = new Intent(MainActivity3.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
+
+
+/*
+試試看能不能：
+1. 每次按下按鈕 魚都出現在不同位置     （寫在java還是xml？？）
+2. 魚一開始是隱形的
+*/
