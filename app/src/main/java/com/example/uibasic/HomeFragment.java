87056647bc3d5db432e93fc599b4c3e023a0b1c7
heya <109306049@nccu.edu.tw>
2023-10-26 12:33:23 +0800

    1026_2 目前可以跑、可以用旁邊的menu到其他頁面 但是主畫面的按鈕不能用:((( （包在inner class裡，結果沒跑它直接跳過去？？？） （大概就是跟註解掉差不多意思==）
    -----
    
    總結這兩天改的：
    1. 把原本在MainActivity.java的程式碼移到HomeFragment.java，MainActivity.java裡面寫的是menu的程式碼
    2. 把沒用到的MainActivity2.java和activity_main2.xml刪掉（AndroidManifest.xml裡有提到的地方也刪掉了）
    3. 把所有名字叫”Settings”的改成”Timer”、叫”Sharing”的改成”Calendar"；也改了nav_menu所以側邊欄的名字也改了但圖案還沒改
    
    -----
    
    提醒自己：
    ＊把沒用到的.gif等都刪掉
    ＊用for loop創魚？！
    ＊把周謙皓的timer程式碼抓下來看看

--- app/src/main/java/com/example/uibasic/HomeFragment.java
+++ app/src/main/java/com/example/uibasic/HomeFragment.java
@@ -24,102 +24,110 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //1026 試試看包進inner class？？？ :'/
+        class try1026 extends AppCompatActivity {
+            ImageButton addFish;
+
+            @Override
+            protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.fragment_home);
+
+                //1019 事先在xml寫好的檔案 一開始先隱藏
+                GifImageView blue_fish_0 = findViewById(R.id.blue_fish_0);
+                blue_fish_0.setVisibility(View.INVISIBLE);
+
+                //0806
+                GifImageView fish_gif = findViewById(R.id.fish_gif);
+                //0821
+                GifImageView fish_gif_2 = findViewById(R.id.fish_gif_2);
+                GifImageView bw_fish = findViewById(R.id.bw_fish);
+                //0827
+                GifImageView bw_fish_2 = findViewById(R.id.bw_fish_2);
+                try {
+                    GifDrawable gifFish = new GifDrawable(getResources(), R.drawable.blue_fish);
+                    fish_gif.setImageDrawable(gifFish);
+                    //0821
+                    GifDrawable gifFish2 = new GifDrawable(getResources(), R.drawable.blue_fish);
+                    fish_gif_2.setImageDrawable(gifFish2);
+                    GifDrawable bwFish = new GifDrawable(getResources(), R.drawable.black_white_fish);
+                    bw_fish.setImageDrawable(bwFish);
+                    //0827
+                    GifDrawable bwFish2 = new GifDrawable(getResources(), R.drawable.black_white_fish);
+                    bw_fish_2.setImageDrawable(bwFish2);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                //0813
+                GifImageView seaweed_gif = findViewById(R.id.seaweed_gif);
+                GifImageView barrel_gif = findViewById(R.id.barrel_gif);
+                GifImageView seaweed_gif_2 = findViewById(R.id.seaweed_gif_2);
+                try {
+                    GifDrawable gifSeaweed = new GifDrawable(getResources(), R.drawable.seaweed_1);
+                    GifDrawable gifBarrel = new GifDrawable(getResources(), R.drawable.barrel_1);
+                    GifDrawable gifSeaweed2 = new GifDrawable(getResources(), R.drawable.seaweed_1);
+                    seaweed_gif.setImageDrawable(gifSeaweed);
+                    barrel_gif.setImageDrawable(gifBarrel);
+                    seaweed_gif_2.setImageDrawable(gifSeaweed2);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+                //0925 用主畫面左邊那個button試做成新增魚的按鈕；重做0807的
+                addFish = findViewById(R.id.imageButton3);
+                addFish.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //1019 一開始先隱藏，點了按鈕之後才會出現   （成功ㄌ！！！:DDD ）
+                        blue_fish_0.setVisibility(View.VISIBLE);
+
+                        blue_fish_0.setX((int) (Math.random() * 1000));
+                        blue_fish_0.setY((int) (Math.random() * 2000));
+                        try {
+                            GifDrawable blueFish0 = new GifDrawable(getResources(), R.drawable.blue_fish);
+                            blue_fish_0.setImageDrawable(blueFish0);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+
+                //0827 按中間那顆可以切到登入畫面
+                ImageButton mdbt = findViewById(R.id.imageButton);
+                mdbt.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), login_page.class);
+                        startActivity(intent);
+                    }
+                });
+
+
+                //1008 右邊那顆切魚
+                ImageButton rtbt = findViewById(R.id.imageButton2);
+                rtbt.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+                        startActivity(intent);
+                    }
+                });
+
+            }
+        }
+
+            //1026  移到下面，不然上面的inner class不會被執行🥲
+            //欸不是，跑是跑得動了，但還是沒有用到啊？？？？？？？
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
 
-        private ImageButton addFish;
-
-        @Override
-        protected void onCreate (Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            //1019 事先在xml寫好的檔案 一開始先隱藏
-            GifImageView blue_fish_0 = findViewById(R.id.blue_fish_0);
-            blue_fish_0.setVisibility(View.INVISIBLE);
-
-            //0806
-            GifImageView fish_gif = findViewById(R.id.fish_gif);
-            //0821
-            GifImageView fish_gif_2 = findViewById(R.id.fish_gif_2);
-            GifImageView bw_fish = findViewById(R.id.bw_fish);
-            //0827
-            GifImageView bw_fish_2 = findViewById(R.id.bw_fish_2);
-            try {
-                GifDrawable gifFish = new GifDrawable(getResources(), R.drawable.blue_fish);
-                fish_gif.setImageDrawable(gifFish);
-                //0821
-                GifDrawable gifFish2 = new GifDrawable(getResources(), R.drawable.blue_fish);
-                fish_gif_2.setImageDrawable(gifFish2);
-                GifDrawable bwFish = new GifDrawable(getResources(), R.drawable.black_white_fish);
-                bw_fish.setImageDrawable(bwFish);
-                //0827
-                GifDrawable bwFish2 = new GifDrawable(getResources(), R.drawable.black_white_fish);
-                bw_fish_2.setImageDrawable(bwFish2);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            //0813
-            GifImageView seaweed_gif = findViewById(R.id.seaweed_gif);
-            GifImageView barrel_gif = findViewById(R.id.barrel_gif);
-            GifImageView seaweed_gif_2 = findViewById(R.id.seaweed_gif_2);
-            try {
-                GifDrawable gifSeaweed = new GifDrawable(getResources(), R.drawable.seaweed_1);
-                GifDrawable gifBarrel = new GifDrawable(getResources(), R.drawable.barrel_1);
-                GifDrawable gifSeaweed2 = new GifDrawable(getResources(), R.drawable.seaweed_1);
-                seaweed_gif.setImageDrawable(gifSeaweed);
-                barrel_gif.setImageDrawable(gifBarrel);
-                seaweed_gif_2.setImageDrawable(gifSeaweed2);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-
-            //0925 用主畫面左邊那個button試做成新增魚的按鈕；重做0807的
-            addFish = findViewById(R.id.imageButton3);
-            addFish.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    //1019 一開始先隱藏，點了按鈕之後才會出現   （成功ㄌ！！！:DDD ）
-                    blue_fish_0.setVisibility(View.VISIBLE);
-
-                    blue_fish_0.setX((int)(Math.random()*1000));
-                    blue_fish_0.setY((int)(Math.random()*2000));
-                    try {
-                        GifDrawable blueFish0 = new GifDrawable(getResources(), R.drawable.blue_fish);
-                        blue_fish_0.setImageDrawable(blueFish0);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-
-
-            //0827 按中間那顆可以切到登入畫面
-            ImageButton mdbt = findViewById(R.id.imageButton);
-            mdbt.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(MainActivity.this, login_page.class);
-                    startActivity(intent);
-                }
-            });
-
-
-            //1008 右邊那顆切魚
-            ImageButton rtbt = findViewById(R.id.imageButton2);
-            rtbt.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(MainActivity.this, MainActivity3.class);
-                    startActivity(intent);
-                }
-            });
-
-        }
-
     }
+
+}
 
 
 
@@ -138,5 +146,3 @@ public class HomeFragment extends Fragment {
 2. 主畫面右邊的按鈕連接新增魚的頁面，點了要新增的魚種就跳回主頁面 *應該要能輸入名稱之類的？？*
 3. 主畫面左邊的按鈕暫作為有新增功能，按一下會顯示出寫好的xml（一開始先隱藏）；出現位置隨機 *所以盡量不要用寫死的gif...🥲*
 */
-    }
-}
