230b70d9467a6f6095006a08adae68a1ef4fbbe6
Chien Hao <108306054@g.nccu.edu.tw>
2023-11-07 02:32:24 +0800

    2023 11 07
    TimerFragment.java/fragment_timer.xml
    番茄鐘完成 使用者可自行輸入想要重複的次數
    每次都是工作25分鐘休息5分鐘總共30分鐘為一次循環
    
    新增boarder.xml
    是timer頁面裡面repetitionEditText外圍的框

--- app/src/main/java/com/example/uibasic/TimerFragment.java
+++ app/src/main/java/com/example/uibasic/TimerFragment.java
@@ -1,50 +1,129 @@
 package com.example.uibasic;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Handler;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class TimerFragment extends Fragment {
+
     private ProgressBar progressBar;
     private TextView progressText;
-    int i = 0;
+    private Button startButton;
+    private CountDownTimer pomodoroTimer;
+    private boolean isRunning = false;
+    private EditText repetitionEditText;
+    private int repetitions;
+    private TextView timerStatusText;
+
+
 
     @Override
-    @Nullable
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_timer, container, false);
 
-        // Find the ProgressBar and TextView within the inflated view
         progressBar = view.findViewById(R.id.progress_bar);
         progressText = view.findViewById(R.id.progress_text);
+        startButton = view.findViewById(R.id.startButton);
+        repetitionEditText = view.findViewById(R.id.repetitionEditText);
+        timerStatusText = view.findViewById(R.id.timerStatusText);
 
-        // Timer
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        startButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                // Set the limitations for the numeric
-                // text under the progress bar
-                if (i <= 100) {
-                    progressText.setText("" + i);
-                    progressBar.setProgress(i);
-                    i++;
-                    handler.postDelayed(this, 600);
+            public void onClick(View v) {
+                if (isRunning) {
+                    // Pause or stop the timer
+                    pomodoroTimer.cancel();
+                    isRunning = false;
+                    startButton.setText("開始計時");
                 } else {
-                    handler.removeCallbacks(this);
+                    // Start or resume the timer
+                    repetitions = Integer.parseInt(repetitionEditText.getText().toString());
+                    startPomodoroTimer();
                 }
             }
-        }, 200);
+        });
 
         return view;
+    }
+
+    private void startPomodoroTimer() {
+        pomodoroTimer = new CountDownTimer(25 * 60 * 1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // Update the circular progress bar
+                int progress = (int) ((25 * 60 * 1000 - millisUntilFinished) * 100 / (25 * 60 * 1000));
+                progressBar.setProgress(progress);
+
+                // Update the UI with the remaining time
+                int minutes = (int) (millisUntilFinished / 1000) / 60;
+                int seconds = (int) (millisUntilFinished / 1000) % 60;
+                progressText.setText(String.format("%02d:%02d", minutes, seconds));
+
+                timerStatusText.setText("工作中");
+            }
+
+            @Override
+            public void onFinish() {
+                // Pomodoro session is over; handle break session or other logic
+                Toast t = Toast.makeText(requireContext(), "開始休息!", Toast.LENGTH_SHORT);
+                t.show();
+                startBreakTimer();
+            }
+        };
+
+        pomodoroTimer.start();
+        isRunning = true;
+        startButton.setText("停止計時");
+    }
+
+    private void startBreakTimer() {
+        pomodoroTimer = new CountDownTimer(5 * 60 * 1000, 1000) { // 5 minutes break timer (adjust as needed)
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // Update the circular progress bar for the break timer
+                int progress = (int) ((5 * 60 * 1000 - millisUntilFinished) * 100 / (5 * 60 * 1000));
+                progressBar.setProgress(progress);
+
+                // Update the UI with the remaining time
+                int minutes = (int) (millisUntilFinished / 1000) / 60;
+                int seconds = (int) (millisUntilFinished / 1000) % 60;
+                progressText.setText(String.format("%02d:%02d", minutes, seconds));
+
+                timerStatusText.setText("休息中");
+            }
+
+            @Override
+            public void onFinish() {
+                // Break session is over; you can either stop the timer or handle the next work session
+                if(repetitions > 1){
+                    Toast t = Toast.makeText(requireContext(), "開始工作!", Toast.LENGTH_SHORT);
+                    t.show();
+                    repetitions--;
+                    startPomodoroTimer();
+                }
+                else {
+                    Toast t = Toast.makeText(requireContext(), "本次工作完成!", Toast.LENGTH_SHORT);
+                    t.show();
+                    isRunning = false;
+                    startButton.setText("開始計時");
+                    timerStatusText.setText("閒置中");
+                }
+            }
+        };
+
+        pomodoroTimer.start();
+        isRunning = true;
+        startButton.setText("停止計時");
     }
 }
