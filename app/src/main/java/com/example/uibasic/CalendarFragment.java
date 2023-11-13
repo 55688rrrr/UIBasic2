c35e19f008fe8c803eab856207bbb021cd4d54dc
55688rrrr <109306039@g.nccu.edu.tw>
2023-11-13 18:01:09 +0800

    +calender

--- app/src/main/java/com/example/uibasic/CalendarFragment.java
+++ app/src/main/java/com/example/uibasic/CalendarFragment.java
@@ -1,5 +1,9 @@
 package com.example.uibasic;
 
+import static com.example.uibasic.CalendarUtils.daysInMonthArray;
+import static com.example.uibasic.CalendarUtils.monthYearFromDate;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,116 +23,106 @@ import java.util.ArrayList;
 
 import androidx.fragment.app.Fragment;
 
-public class CalendarFragment extends Fragment implements CalendarAdapter.OnItemListener
-{
-    private TextView monthYearText;
-    private RecyclerView calendarRecyclerView;
-    private LocalDate selectedDate;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-    {
+public class CalendarFragment extends Fragment implements CalendarAdapter.OnItemListener
+
+
+
+        {
+private TextView monthYearText;
+private RecyclerView calendarRecyclerView;
+
+private LocalDate selectedDate;
+
+@Override
+public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
 
         View view = inflater.inflate(R.layout.fragment_calendar, container, false);
 
         initWidgets(view);
-        selectedDate = LocalDate.now();
+        CalendarUtils.selectedDate = LocalDate.now();
         setMonthView();
 
         Button previousButton = view.findViewById(R.id.previousButton);
         Button nextButton = view.findViewById(R.id.nextButton);
+        Button weeklyAction = view.findViewById(R.id.weeklyAction);
 
         previousButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                previousMonthAction(v);
-            }
+@Override
+public void onClick(View v) {
+        previousMonthAction(v);
+        }
         });
 
         nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                nextMonthAction(v);
-            }
+@Override
+public void onClick(View v) {
+        nextMonthAction(v);
+        }
+        });
+        weeklyAction.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+        weeklyAction(v);
+        }
         });
 
         return view;
-    }
+        }
 
-    //Implement each method you want to use.
+//Implement each method you want to use.
 //    public String findViewById(int id);
 //    {
 //        return appCompatActivity.findViewById(id);
 //    }
 
-    private void initWidgets(View view)
-    {
+private void initWidgets(View view)
+        {
 //        calendarRecyclerView = appCompatActivity.findViewById(R.id.calendarRecyclerView);
 //        monthYearText = appCompatActivity.findViewById(R.id.monthYearTV);
         calendarRecyclerView = view.findViewById(R.id.calendarRecyclerView);
         monthYearText = view.findViewById(R.id.monthYearTV);
-    }
+        }
 
-    private void setMonthView()
-    {
-        monthYearText.setText(monthYearFromDate(selectedDate));
-        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
-
+private void setMonthView()
+        {
+        monthYearText.setText(monthYearFromDate(CalendarUtils.selectedDate));
+        ArrayList<LocalDate> daysInMonth = daysInMonthArray(CalendarUtils.selectedDate);
+        System.out.println(daysInMonth);
         CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(requireContext(), 7);
         calendarRecyclerView.setLayoutManager(layoutManager);
         calendarRecyclerView.setAdapter(calendarAdapter);
-    }
-
-    private ArrayList<String> daysInMonthArray(LocalDate date)
-    {
-        ArrayList<String> daysInMonthArray = new ArrayList<>();
-        YearMonth yearMonth = YearMonth.from(date);
-
-        int daysInMonth = yearMonth.lengthOfMonth();
-
-        LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
-        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
-
-        for(int i = 1; i <= 42; i++)
-        {
-            if(i <= dayOfWeek || i > daysInMonth + dayOfWeek)
-            {
-                daysInMonthArray.add("");
-            }
-            else
-            {
-                daysInMonthArray.add(String.valueOf(i - dayOfWeek));
-            }
         }
-        return  daysInMonthArray;
-    }
 
-    private String monthYearFromDate(LocalDate date)
-    {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
-        return date.format(formatter);
-    }
 
-    public void previousMonthAction(View view)
-    {
-        selectedDate = selectedDate.minusMonths(1);
-        setMonthView();
-    }
 
-    public void nextMonthAction(View view)
-    {
-        selectedDate = selectedDate.plusMonths(1);
-        setMonthView();
-    }
-
-    @Override
-    public void onItemClick(int position, String dayText)
-    {
-        if(!dayText.equals(""))
+public void previousMonthAction(View view)
         {
-            String message = "Selected Date " + dayText + " " + monthYearFromDate(selectedDate);
-            Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show();
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusMonths(1);
+        setMonthView();
         }
-    }
-}
+
+public void nextMonthAction(View view)
+        {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusMonths(1);
+        setMonthView();
+        }
+
+
+@Override
+public void onItemClick(int position, LocalDate date)
+        {
+        if(date != null)
+        {
+        CalendarUtils.selectedDate = date;
+        setMonthView();
+        }
+        }
+
+
+public void weeklyAction(View view) {
+        Intent intent = new Intent(requireContext(), WeekViewActivity.class);
+        startActivity(intent);    }
+        }
