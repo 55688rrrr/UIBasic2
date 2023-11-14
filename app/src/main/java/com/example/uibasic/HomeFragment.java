1f03214037d7e9f1b4169aa077146bc230cc834a
高昂 <108306009@nccu.edu.tw>
2023-11-15 00:44:38 +0800

    新增功能完成(主畫面未更新)

--- app/src/main/java/com/example/uibasic/HomeFragment.java
+++ app/src/main/java/com/example/uibasic/HomeFragment.java
@@ -1,25 +1,50 @@
 package com.example.uibasic;
 
+import static com.example.uibasic.JsonUtils.parseJsonToDailyList;
+import static com.example.uibasic.JsonUtils.readJsonFromFile;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.view.LayoutInflater;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager.widget.ViewPager;
 
 //0806
 import pl.droidsonroids.gif.GifImageView;
 import pl.droidsonroids.gif.GifDrawable;
 //0807
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
 //????
 import android.content.Intent;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.tabs.TabLayout;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
+import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -30,7 +55,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class HomeFragment extends Fragment {
 
@@ -42,6 +71,12 @@ public class HomeFragment extends Fragment {
     private Connection con;
 
     private View view;
+
+    private String BiguserId;
+
+    //private Context context;
+
+    //private EditText missionDeadlineInput;
 
 
     //private FrameLayout container;
@@ -64,36 +99,45 @@ public class HomeFragment extends Fragment {
         GifImageView barrel_gif = view.findViewById(R.id.barrel_gif);
         GifImageView seaweed_gif_2 = view.findViewById(R.id.seaweed_gif_2);
 
+
         //json
         // 讀取 JSON 檔案中的 daily 資料
-        String jsonDailyData = readJsonFromFile("daily_data.json");
+        String jsonDailyData = JsonUtils.readJsonFromFile(getActivity(),"daily_data.json");
         // 解析 JSON 字串為 ArrayList<Daily> 物件
-        ArrayList<Daily> dailyList = parseJsonToDailyList(jsonDailyData);
+        ArrayList<Daily> dailyList = JsonUtils.parseJsonToDailyList(jsonDailyData);
+        int lastIndex_daily = dailyList.size() - 1;
+
+        BiguserId= dailyList.get(0).getUserId();
+
         System.out.println("jsonnnnnnnnnnnnn "+ dailyList.size());
         System.out.println("jsonnnnnnnnnnnnn "+ dailyList.get(0).getDaily_id());
 
         // 讀取 JSON 檔案中的 eat 資料
-        String jsonEatData = readJsonFromFile("eat_data.json");
+        String jsonEatData = JsonUtils.readJsonFromFile(getActivity(),"eat_data.json");
         // 解析 JSON 字串為 ArrayList<Eat> 物件
-        ArrayList<Eat> eatList = parseJsonToEatList(jsonEatData);
+        ArrayList<Eat> eatList = JsonUtils.parseJsonToEatList(jsonEatData);
+        int lastIndex_eat = eatList.size() - 1;
         System.out.println("jsonnnnnnnnnnnnn "+ eatList.size());
 
         // 讀取 JSON 檔案中的 health 資料
-        String jsonHealthData = readJsonFromFile("health_data.json");
+        String jsonHealthData = JsonUtils.readJsonFromFile(getActivity(),"health_data.json");
         // 解析 JSON 字串為 ArrayList<Health> 物件
-        ArrayList<Health> healthList = parseJsonToHealthList(jsonHealthData);
+        ArrayList<Health> healthList = JsonUtils.parseJsonToHealthList(jsonHealthData);
+        int lastIndex_health = healthList.size() - 1;
         System.out.println("jsonnnnnnnnnnnnn "+ healthList.size());
 
         // 讀取 JSON 檔案中的 type1 資料
-        String jsonType1Data = readJsonFromFile("type1_data.json");
+        String jsonType1Data = JsonUtils.readJsonFromFile(getActivity(),"type1_data.json");
         // 解析 JSON 字串為 ArrayList<Type1> 物件
-        ArrayList<Type1> type1List = parseJsonToType1List(jsonType1Data);
+        ArrayList<Type1> type1List = JsonUtils.parseJsonToType1List(jsonType1Data);
+        int lastIndex_type1 = type1List.size() - 1;
         System.out.println("jsonnnnnnnnnnnnn "+ type1List.size());
 
         // 讀取 JSON 檔案中的 type2 資料
-        String jsonType2Data = readJsonFromFile("type2_data.json");
+        String jsonType2Data = JsonUtils.readJsonFromFile(getActivity(),"type2_data.json");
         // 解析 JSON 字串為 ArrayList<Type2> 物件
-        ArrayList<Type2> type2List = parseJsonToType2List(jsonType2Data);
+        ArrayList<Type2> type2List = JsonUtils.parseJsonToType2List(jsonType2Data);
+        int lastIndex_type2 = type2List.size() - 1;
         System.out.println("jsonnnnnnnnnnnnn "+ type2List.size());
 
 
@@ -245,6 +289,7 @@ public class HomeFragment extends Fragment {
             }
         });*/
 
+        //feed
         ImageButton mdbt = view.findViewById(R.id.imageButton);
         mdbt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -256,6 +301,8 @@ public class HomeFragment extends Fragment {
             }
         });
 
+
+        //share
         ImageButton rtbt = view.findViewById(R.id.imageButton2);
         rtbt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -266,10 +313,228 @@ public class HomeFragment extends Fragment {
         });
 
 
+
+
+        //new fish
+        ImageButton new_fish = view.findViewById(R.id.imageButton3);
+        new_fish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // 創建 BottomSheetDialog
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(getContext());
+                bottomSheetDialog.setContentView(R.layout.bottom_sheet_dialog_layout);
+
+
+
+                // 找到布局中的元件
+                //CheckBox checkBox1 = bottomSheetDialog.findViewById(R.id.checkBox1);
+                CheckBox type2Check = bottomSheetDialog.findViewById(R.id.type2Check);
+                Button confirmButton = bottomSheetDialog.findViewById(R.id.confirmButton);
+                EditText missionNameInput = bottomSheetDialog.findViewById(R.id.missionNameInput);
+                EditText missionGoalInput = bottomSheetDialog.findViewById(R.id.missionGoalInput);
+                EditText missionDeadlineInput = bottomSheetDialog.findViewById(R.id.missionDeadlineInput);
+                TextView missionName = bottomSheetDialog.findViewById(R.id.missionName);
+                TextView missionGoal = bottomSheetDialog.findViewById(R.id.missionGoal);
+                TextView missionDeadline = bottomSheetDialog.findViewById(R.id.missionDeadline);
+
+                // 找到布局中的 Spinner
+                Spinner spinnerTaskType = bottomSheetDialog.findViewById(R.id.spinnerTaskType);
+
+                // 設置 Spinner 選項
+                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.task_types, android.R.layout.simple_spinner_item);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinnerTaskType.setAdapter(adapter);
+
+                /*
+                // 找到布局中的相关控件
+                missionDeadlineInput = view.findViewById(R.id.missionDeadlineInput);
+
+                // 添加点击事件监听器
+                missionDeadlineInput.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showDatePickerDialog();
+                    }
+                });*/
+
+
+                // 添加 Spinner 選擇監聽事件
+                spinnerTaskType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                        // 根據 Spinner 選擇的項目，動態顯示或隱藏相應的輸入框和按鈕
+                        switch (position) {
+                            case 0:
+                            case 1:
+                            case 2:
+                                // 顯示或隱藏相應的元件，這裡以 CheckBox 為例
+                                type2Check.setVisibility(View.GONE);
+                                missionNameInput.setVisibility(View.VISIBLE);
+                                missionGoalInput.setVisibility(View.VISIBLE);
+                                missionDeadlineInput.setVisibility(View.GONE);
+                                missionName.setVisibility(View.VISIBLE);
+                                missionGoal.setVisibility(View.VISIBLE);
+                                missionDeadline.setVisibility(View.GONE);
+                                break;
+                            case 3:
+                                type2Check.setVisibility(View.VISIBLE);
+                                // 添加 CheckBox 監聽事件
+                                type2Check.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                                    @Override
+                                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                                        // 根據 CheckBox 的狀態，顯示或隱藏相應的輸入框
+                                        if (isChecked) {
+                                            missionNameInput.setVisibility(View.VISIBLE);
+                                            missionGoalInput.setVisibility(View.GONE);
+                                            missionDeadlineInput.setVisibility(View.VISIBLE);
+                                            missionName.setVisibility(View.VISIBLE);
+                                            missionGoal.setVisibility(View.GONE);
+                                            missionDeadline.setVisibility(View.VISIBLE);
+                                        } else {
+                                            missionNameInput.setVisibility(View.VISIBLE);
+                                            missionGoalInput.setVisibility(View.VISIBLE);
+                                            missionDeadlineInput.setVisibility(View.GONE);
+                                            missionName.setVisibility(View.VISIBLE);
+                                            missionGoal.setVisibility(View.VISIBLE);
+                                            missionDeadline.setVisibility(View.GONE);
+                                        }
+                                    }
+                                });
+
+                                /*case 4:
+                                // 顯示或隱藏相應的元件，這裡以 CheckBox 為例
+                                missionNameInput.setVisibility(View.VISIBLE);
+                                missionGoalInput.setVisibility(View.GONE);
+                                missionDeadlineInput.setVisibility(View.VISIBLE);
+                                missionName.setVisibility(View.VISIBLE);
+                                missionGoal.setVisibility(View.GONE);
+                                missionDeadline.setVisibility(View.VISIBLE);
+                                break;*/
+                            // 添加更多選項的處理邏輯
+                        }
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parentView) {
+                        // 在這裡處理沒有選擇任何項目的情況
+                    }
+                });
+
+                // 添加確認按鈕監聽事件
+                confirmButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // 在這裡處理確認按鈕的邏輯
+                        // 获取用户输入的数据
+                        String missionNameText = missionNameInput.getText().toString();
+                        //Integer missionGoalText = Integer.valueOf(missionGoalInput.getText().toString());
+                        Integer missionGoalText=0;
+                        String missionGoalInputText = missionGoalInput.getText().toString();
+                        if (!missionGoalInputText.isEmpty()) {
+                            missionGoalText = Integer.valueOf(missionGoalInputText);
+                            // 接下来的逻辑
+                        } else {
+                            // 处理字符串为空的情况
+                        }
+                        String missionDeadlineText = missionDeadlineInput.getText().toString();
+                        boolean isType2Checked = type2Check.isChecked();
+                        Date currentDate = new Date();
+                        // 创建 SimpleDateFormat 对象，指定日期格式
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+                        // 将 Date 对象转换为字符串
+                        String dateString = dateFormat.format(currentDate);
+
+                        // 根据 Spinner 选项确定数据类型
+                        String selectedType = spinnerTaskType.getSelectedItem().toString();
+
+                        // 创建相应的数据对象
+                        if ("日常".equals(selectedType) && !isType2Checked) {
+                            // 创建 Daily 对象
+                            Daily newDaily = new Daily(BiguserId,dailyList.get(lastIndex_daily).getDaily_id()+1, missionNameText, missionGoalText, 0, dateString);
+
+                            // 将新数据添加到列表
+                            dailyList.add(newDaily);
+                            JsonUtils.saveDataToJsonFile(getActivity(), "daily_data.json", dailyList);
+
+                        } else if ("飲食".equals(selectedType)) {
+                            // 类似地，创建 Eat 对象，并处理 Eat 数据
+
+                            Eat newEat = new Eat(BiguserId,eatList.get(lastIndex_eat).getEat_id()+1, missionNameText, missionGoalText, 0, dateString);
+
+                            // 将新数据添加到列表
+                            eatList.add(newEat);
+                            JsonUtils.saveDataToJsonFile(getActivity(), "eat_data.json", eatList);
+
+                        } else if ("健康".equals(selectedType)) {
+                            // 类似地，创建 Health 对象，并处理 Health 数据
+
+                            Health newHealth = new Health(BiguserId,healthList.get(lastIndex_health).getHealth_id()+1, missionNameText, missionGoalText, 0, dateString);
+
+                            // 将新数据添加到列表
+                            healthList.add(newHealth);
+                            JsonUtils.saveDataToJsonFile(getActivity(), "health_data.json", healthList);
+
+                        } else if ("其他".equals(selectedType)) {
+                            // 类似地，创建 Type2 对象，并处理 Type2 数据
+
+                            Type2 newType2 = new Type2(BiguserId,type2List.get(lastIndex_type2).getType2_id()+1, missionNameText, missionGoalText, 0, dateString);
+
+                            // 将新数据添加到列表
+                            type2List.add(newType2);
+                            JsonUtils.saveDataToJsonFile(getActivity(), "type2_data.json", type2List);
+
+                        }else {
+                            // 类似地，创建 Type2 对象，并处理 Type2 数据
+
+                            Type1 newType1 = new Type1(BiguserId,type1List.get(lastIndex_type1).getType1_id()+1, missionNameText, missionDeadlineText, 0, dateString);
+
+                            // 将新数据添加到列表
+                            type1List.add(newType1);
+                            JsonUtils.saveDataToJsonFile(getActivity(), "type1_data.json", type1List);
+                        }
+
+
+                        // 讀取 JSON 檔案中的 daily 資料
+                        String jsonDailyData = JsonUtils.readJsonFromFile(getActivity(),"daily_data.json");
+                        // 解析 JSON 字串為 ArrayList<Daily> 物件
+                        ArrayList<Daily> dailyList777 = parseJsonToDailyList(jsonDailyData);
+                        System.out.println(dailyList777.get(lastIndex_daily+1).getDaily_name()+"oooooooooooooooo");
+                        // 關閉 BottomSheetDialog
+                        bottomSheetDialog.dismiss();
+                    }
+                });
+
+                // 在 show 之前設置 BottomSheetDialog 的高度
+                bottomSheetDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                    @Override
+                    public void onShow(DialogInterface dialog) {
+                        // 在這裡設置 BottomSheetDialog 的高度
+                        View bottomSheetView = bottomSheetDialog.findViewById(R.id.design_bottom_sheet);
+                        if (bottomSheetView != null) {
+                            BottomSheetBehavior.from(bottomSheetView).setPeekHeight(
+                                    getResources().getDimensionPixelSize(R.dimen.bottom_sheet_dialog_height)
+                            );
+                        }
+                    }
+                });
+
+
+                // 顯示 BottomSheetDialog
+
+                bottomSheetDialog.show();
+
+            }
+        });
+
+
         return view;
     }
 
 
+
+    /*
     private String readJsonFromFile(String fileName) {
         String json = "";
         try (InputStream inputStream = getActivity().openFileInput(fileName);
@@ -315,7 +580,32 @@ public class HomeFragment extends Fragment {
         Gson gson = new Gson();
         Type listType = new TypeToken<ArrayList<Type2>>() {}.getType();
         return gson.fromJson(json, listType);
-    }
+    }*/
+
+    /*
+    private void showDatePickerDialog() {
+        // 使用当前日期作为 DatePickerDialog 的默认值
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        // 创建 DatePickerDialog 并设置选择日期的监听器
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                requireContext(),  // 使用当前 Fragment 的 Context
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int selectedYear, int selectedMonth, int selectedDay) {
+                        // 将选择的日期设置到 EditText
+                        missionDeadlineInput.setText(String.format(Locale.getDefault(), "%d/%02d/%02d", selectedYear, selectedMonth + 1, selectedDay));
+                    }
+                },
+                year, month, day);
+
+        // 显示 DatePickerDialog
+        datePickerDialog.show();
+    }*/
+
 }
 
 /*public class HomeFragment extends Fragment {
